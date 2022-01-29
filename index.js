24b3ebe4dc682dfce5bf9ccd12a7909923cad51e
Raghav Khullar <77584466+RaghavKhullar@users.noreply.github.com>
2022-01-29 17:08:59 +0530

    added dropdown

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function addItem() {
   let elementType = document.getElementById("elementType").value;
-
+console.log(elementType)
   let elementValue = document.getElementById("value").value;
 
   elementType = document.createElement(elementType);
+  console.log(elementType)
+  let color = document.getElementById("color").value;
+
 
   elementType.appendChild(document.createTextNode(elementValue));
 
-  document.getElementById("parent").appendChild(elementType);
+  //   document.getElementById("parent").appendChild(elementType);
+
+  document.getElementById("userbody").appendChild(elementType).style.color = color;
 }
